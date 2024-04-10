d212e9c5592f7ed1acee46ecde67d09cc007009c
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-10 15:45:12 -0400

    Second Page and A Link

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+import { Metadata } from '@redwoodjs/web'
+
+const HomePage = () => {
+  return (
+    <>
+      <Metadata title="Home" description="Home page" />
+
+      <header>
+        <h1>Redwood Blog</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
