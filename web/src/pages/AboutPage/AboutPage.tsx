12fe71e1b61e8d288ea15a1b15bf4cd239ea6f42
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-10 16:26:34 -0400

    DB Schema and migration

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -1,18 +1,14 @@
-import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
 const AboutPage = () => {
   return (
     <>
-      <>
-        <Metadata title="About" description="About page" />
+      <Metadata title="About" description="About page" />
 
-        <p>
-          This site was created to demonstrate my mastery of Redwood: Look on my
-          works, ye mighty, and despair!
-        </p>
-        <Link to={routes.home()}>Return home</Link>
-      </>
+      <p>
+        This site was created to demonstrate my mastery of Redwood: Look on my
+        works, ye mighty, and despair!
+      </p>
     </>
   )
 }
