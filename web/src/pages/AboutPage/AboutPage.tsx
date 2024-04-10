196a2fdaf13a48d4765b10a48715c0b7183accaa
Sandeep Joshi <ysandeepjoshi@gmail.com>
2024-04-10 16:05:24 -0400

    Blog layout

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -4,25 +4,15 @@ import { Metadata } from '@redwoodjs/web'
 const AboutPage = () => {
   return (
     <>
-      <Metadata title="About" description="About page" />
+      <>
+        <Metadata title="About" description="About page" />
 
-      <header>
-        <h1>Redwood Blog</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>
         <p>
           This site was created to demonstrate my mastery of Redwood: Look on my
           works, ye mighty, and despair!
         </p>
         <Link to={routes.home()}>Return home</Link>
-      </main>
+      </>
     </>
   )
 }
